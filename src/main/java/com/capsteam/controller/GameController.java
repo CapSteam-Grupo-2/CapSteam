4996e837db6b946109cf9190db7920ca36fd6078
manuelherreragonzalez <manuel.herrera-gonzalez@capgemini.com>
2023-03-23 11:19:59 +0100

    add delete method

--- src/main/java/com/capsteam/controller/GameController.java
+++ src/main/java/com/capsteam/controller/GameController.java
@@ -2,9 +2,11 @@ package com.capsteam.controller;
 
 import com.capsteam.model.GameModel;
 import com.capsteam.service.GameService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,7 @@ public class GameController {
 	@Autowired
 	private GameService service;
 
+
 	@GetMapping("/edit")
 	public String editGame(@RequestParam("id") int id, Model model) {
 		model.addAttribute("game", service.findById(id));
@@ -23,7 +26,7 @@ public class GameController {
 
 	@PostMapping("/save")
 	public String saveGame(GameModel game) {
-		service.saveGame(game);
+		service.save(game);
 		return ("redirect:/");
 	}
 
@@ -31,6 +34,13 @@ public class GameController {
 	public String addGame(GameModel game, Model model) {
 		model.addAttribute("game", game);
 		return "form.html";
+
+	}
+
+	@DeleteMapping("/delete")
+	public String removeGame(@RequestParam("id") int id) {
+		service.deleteById(id);
+		return ("redirect:/");
 
 	}
 }
