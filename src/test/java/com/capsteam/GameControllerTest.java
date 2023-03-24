8af097946c1e2f1968a588e8e48303394bb745c0
Dcl <david.chenliang-lu@capgemini.com>
2023-03-24 11:46:06 +0100

    a

--- src/test/java/com/capsteam/GameControllerTest.java
+++ src/test/java/com/capsteam/GameControllerTest.java
@@ -1,37 +1,24 @@
 package com.capsteam;
 
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertNotEquals;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ui.Model;
 
 import com.capsteam.model.GameModel;
 import com.capsteam.service.GameService;
 
 @SpringBootTest
 public class GameControllerTest {
-	
-/*
-	Autowired
-	GameService service;
-	
-	
-	 	void listGames() throws Exception {
-	 		
-	 	List<GameModel> games;
-	 	when(service.getGames()).thenReturn( games);
-	 	
-		mockMvc.perform(get("/css/estilos.css"))
-			   .andDo(print())
-			   .andExpect(status().isOk());
+
+	@Autowired
+	private GameService service;
+
+	void listGames() throws Exception {
+		List<GameModel> list;
+		assertNotEquals(null, service.getGames());
+
 	}
-	*/
-	
 }
